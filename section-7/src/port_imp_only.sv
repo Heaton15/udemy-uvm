c7830c49ac74dd86eb0c0bfdcf02e7b3f5bb083f
Tim Heaton <tim.heaton@niobiummicrosystems.com>
2023-07-04 09:23:03 -0700

    This testbench demos how you can go from producer to imp and skip the consumer

--- section-7/src/port_imp_only.sv
+++ section-7/src/port_imp_only.sv
@@ -24,7 +24,7 @@ class producer_1 extends uvm_component;
   task main_phase(uvm_phase phase);
     phase.raise_objection(this);
     send.put(data);
-    `uvm_info("PROD", $sformatf("Data Sent: %0d", data), UVM_NONE);
+    `uvm_info("producer_1", $sformatf("Data Sent: %0d", data), UVM_NONE);
     phase.drop_objection(this);
   endtask
 endclass
@@ -32,7 +32,8 @@ endclass
 class consumer_1 extends uvm_component;
   `uvm_component_utils(consumer_1)
 
-  uvm_blocking_put_export #(int) recv;
+  /* We can comment out the export block and go straight from port to imp */
+  //uvm_blocking_put_export #(int) recv;
 
   // Wherever you want to receive the data is where the implementation should go
   // We need to define a put task in class consumer_1 
@@ -44,8 +45,9 @@ class consumer_1 extends uvm_component;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    recv = new("recv", this);
-    imp  = new("imp", this);
+    /* We can comment out the export block and go straight from port to imp */
+    //recv = new("recv", this);
+    imp = new("imp", this);
   endfunction
 
   task put(int datar);
@@ -54,13 +56,13 @@ class consumer_1 extends uvm_component;
 
 endclass
 
-class env_0 extends uvm_env;
-  `uvm_component_utils(env_0)
+class env_1 extends uvm_env;
+  `uvm_component_utils(env_1)
 
   producer_1 p;
   consumer_1 c;
 
-  function new(input string path = "env_0", uvm_component parent = null);
+  function new(input string path = "env_1", uvm_component parent = null);
     super.new(path, parent);
   endfunction
 
@@ -73,14 +75,17 @@ class env_0 extends uvm_env;
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
 
-    p.send.connect(c.recv);
-    c.recv.connect(c.imp);
+    p.send.connect(c.imp);
+
+    /* We can comment out the export block and go straight from port to imp */
+    //p.send.connect(c.recv);
+    //c.recv.connect(c.imp);
   endfunction
 endclass
 
-class test_port_export_imp extends uvm_test;
-  `uvm_component_utils(test_port_export_imp)
-  env_0 e;
+class test_port_imp_only extends uvm_test;
+  `uvm_component_utils(test_port_imp_only)
+  env_1 e;
 
   function new(input string path = "test", uvm_component parent = null);
     super.new(path, parent);
@@ -88,6 +93,6 @@ class test_port_export_imp extends uvm_test;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    e = env_0::type_id::create("e", this);
+    e = env_1::type_id::create("e", this);
   endfunction
 endclass
